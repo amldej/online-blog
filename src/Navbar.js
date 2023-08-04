344e96de5467a91534b1b8758b8dfb16a6a08bf7
amldej <angelicamldej@gmail.com>
2023-08-04 10:19:50 +0100

    phone view

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => {
 	return (
 		<div>
 		<nav className="navbar">
-		<div><Link to="/online-blog">home</Link></div>
+		<div style={{paddingLeft: 20}}><Link to="/online-blog">home</Link></div>
 		<div className="logo">yours truly</div>
-			<div className="links">
+			<div style={{paddingRight: 20}} className="links">
 				<Link to="/thoughts">thoughts</Link>
 				<Link to="/create">new</Link>
 			</div>
